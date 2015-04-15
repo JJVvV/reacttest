17a05995ac4980fa64742825717108a7bf7c6df8
JJVvV <drowning524@gmail.com>
2015-04-16 03:21:10 +0800

    test

---
+++ js/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2015/4/16.
+ */
+var React = require('react');
+
+React.render(<span>asdfa</span>, document.getElementById('example'));
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+///**
+// * Created by Administrator on 2015/4/16.
+// */
+//var path = require('path');
+//var webpack = require('webpack');
+//console.log(path.join(__dirname, 'build'));
+//module.exports = {
+//    entry:[
+//        'webpack-dev-server/client?http://localhost:3000',
+//        'webpack/hot/only-dev-server',
+//        './js/app.js'
+//    ],
+//    output: {
+//        path: path.join(__dirname, 'build'),
+//        filename: 'bundle.js',
+//        publicPath: '/js/'
+//    },
+//    plugins: [
+//        new webpack.HotModuleReplacementPlugin(),
+//        new webpack.NoErrorsPlugin()
+//    ],
+//    resolve: {
+//        extensions: ['', '.js', '.jsx']
+//    },
+//    module: {
+//        loaders: [
+//            { test: /\.jsx$/, loader: ['react-hot', 'babel'] }
+//        ]
+//    }
+//}
+
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './js/app'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/js/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loaders: ['react-hot', 'babel'],
+            include: path.join(__dirname, 'js')
+        }]
+    }
+};
